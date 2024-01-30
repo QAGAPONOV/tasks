524c8a218e28c74ec15211782c61537abe571719
Alexander <gaponovqa@gmail.com>
2024-01-30 12:29:18 +0300

    добавил цикл

--- src/main/java/Reverse.java
+++ src/main/java/Reverse.java
@@ -1,7 +1,25 @@
 //Пeревернуть строку без использования методов типа reverse используя один массив.
 //Пример: myReverse("12345"). Результат: "54321"
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Reverse {
+    //    public static void main(String[] args) {
+//        String line = "12345";
+//        getReverseLine(line);
+//    }
+//
+//    public static void getReverseLine(String sourceLine) {
+//        char[] array = sourceLine.toCharArray();
+//        for (int i = 0, j = (array.length - 1); i < array.length/2; i++, j--){
+//            char variable = array[i];
+//            array[i] = array[j];
+//            array[j] = variable;
+//        }
+//        System.out.println(Arrays.toString(array));
+//    }
+//}
     public static void main(String[] args) {
         String myReverse = "12345";
         char[] charArray = myReverse.toCharArray();
@@ -20,3 +38,7 @@ public class Reverse {
         System.out.println(revers);
     }
 }
+
+
+
+
