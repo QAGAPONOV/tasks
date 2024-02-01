afa02260f39fd2a9cdf54dbc477cafe40edcd90e
Alexander <gaponovqa@gmail.com>
2024-02-01 15:52:03 +0300

    Dublicates 2 решение добавлен цикл

--- src/main/java/Dublicates.java
+++ src/main/java/Dublicates.java
@@ -22,28 +22,34 @@ import java.util.*;
 //    }
 //}
 
+
 public class Dublicates {
     public static void main(String[] args) {
         List<Integer> listDublicates = new ArrayList<>();
-        listDublicates.add(1);
-        listDublicates.add(2);
-        listDublicates.add(2);
-        listDublicates.add(3);
-        listDublicates.add(4);
-        listDublicates.add(4);
-        listDublicates.add(4);
-        listDublicates.add(5);
-        listDublicates.add(6);
-        listDublicates.add(7);
-        listDublicates.add(8);
-        listDublicates.add(8);
-        listDublicates.add(9);
-        listDublicates.add(9);
-        listDublicates.add(10);
-        //System.out.println("Список с повторяющимися значениями" + listDublicates);
+//        listDublicates.add(1);
+//        listDublicates.add(2);
+//        listDublicates.add(2);
+//        listDublicates.add(3);
+//        listDublicates.add(4);
+//        listDublicates.add(4);
+//        listDublicates.add(4);
+//        listDublicates.add(5);
+//        listDublicates.add(6);
+//        listDublicates.add(7);
+//        listDublicates.add(8);
+//        listDublicates.add(8);
+//        listDublicates.add(9);
+//        listDublicates.add(9);
+//        listDublicates.add(10);
+        int[] sortedList = {1, 2, 2, 3, 4, 4, 4, 5, 6, 7, 8, 8, 9, 9, 10};
+        for (int i : sortedList) {
+            listDublicates.add(i);
+        }
 
+        System.out.println("Список с повторяющимися значениями" + listDublicates);
         Set<Integer> setListOutDublicates = new HashSet<>(listDublicates);
-        List<Integer> listOutDublicates = new ArrayList<>(setListOutDublicates);
+        //List<Integer> listOutDublicates = new ArrayList<>(setListOutDublicates);
         System.out.println("Без повторяющихся значений" + setListOutDublicates);
     }
 }
+
